9e07bd3d1106405624ddac32ba0a48386e42d4d9
Marcio Araujo <araujo.marcio@pm.me>
2023-03-02 17:30:37 -0300

    [Entrega]-requisito 2 e 3
    Adicionando Campos de email, senha e botão de login

--- src/pages/LoginScreen.jsx
+++ src/pages/LoginScreen.jsx
@@ -2,7 +2,41 @@ import React from 'react';
 
 function LoginScreen() {
   return (
-    <h1>teste</h1>
+    <form>
+
+      <label
+        htmlFor="email"
+      >
+        Email:
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Digite o e-mail"
+          data-testid="email-input"
+        />
+      </label>
+
+      <label
+        htmlFor="password"
+      >
+        Senha:
+        <input
+          type="password"
+          name="password"
+          id="password"
+          data-testid="password-input"
+          placeholder="Digite a senha"
+        />
+      </label>
+
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Enter
+      </button>
+    </form>
   );
 }
 
