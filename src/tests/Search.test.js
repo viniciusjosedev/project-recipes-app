efdc0aed8016a9c261e9a6bfd71e6ca009f10167
felipe <werer522p@gmail.com>
2023-03-03 16:07:29 -0300

    [Entrega] - Cobertura de testes searchBar 100% - Requisito 12 e 15 2.0

--- src/tests/Search.test.js
+++ src/tests/Search.test.js
@@ -6,14 +6,18 @@ import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 import DefaultProvider from '../context/DefaultProvider';
 
+const searchTopBtn = 'search-top-btn';
+const searchInput = 'search-input';
+const exercBtn = 'exec-search-btn';
+
 describe('All tests from Search', () => {
   it('All tests', async () => {
     renderWithRouter(<DefaultProvider><App /></DefaultProvider>, { initialEntries: ['/meals'] });
-    userEvent.click(screen.getByTestId('search-top-btn'));
-    userEvent.type(screen.getByTestId('search-input'), 'chicken');
+    userEvent.click(screen.getByTestId(searchTopBtn));
+    userEvent.type(screen.getByTestId(searchInput), 'chicken');
     userEvent.click(screen.getByLabelText('nome'));
     userEvent.click(screen.getByLabelText('ingrediente'));
-    userEvent.click(screen.getByTestId('exec-search-btn'));
+    userEvent.click(screen.getByTestId(exercBtn));
 
     await wait(1000);
     const chikenResult = screen.getByRole('img', {
@@ -23,10 +27,10 @@ describe('All tests from Search', () => {
   });
   it('More tests', async () => {
     renderWithRouter(<DefaultProvider><App /></DefaultProvider>, { initialEntries: ['/meals'] });
-    userEvent.click(screen.getByTestId('search-top-btn'));
-    userEvent.type(screen.getByTestId('search-input'), 'chicken');
+    userEvent.click(screen.getByTestId(searchTopBtn));
+    userEvent.type(screen.getByTestId(searchInput), 'chicken');
     userEvent.click(screen.getByLabelText('primeira letra'));
-    userEvent.click(screen.getByTestId('exec-search-btn'));
+    userEvent.click(screen.getByTestId(exercBtn));
 
     jest.spyOn(global, 'alert').mockReturnValue('Your search must have only 1 (one) characterxablau');
 
@@ -34,10 +38,10 @@ describe('All tests from Search', () => {
   });
   it('More tests', async () => {
     renderWithRouter(<DefaultProvider><App /></DefaultProvider>, { initialEntries: ['/meals'] });
-    userEvent.click(screen.getByTestId('search-top-btn'));
-    userEvent.type(screen.getByTestId('search-input'), 'ashuaushahihausas');
+    userEvent.click(screen.getByTestId(searchTopBtn));
+    userEvent.type(screen.getByTestId(searchInput), 'ashuaushahihausas');
     userEvent.click(screen.getByLabelText('nome'));
-    userEvent.click(screen.getByTestId('exec-search-btn'));
+    userEvent.click(screen.getByTestId(exercBtn));
 
     jest.spyOn(global, 'alert').mockReturnValue('Sorry, we haven\'t found any recipes for these filters.');
 
