13be5bc3303666e7ee1e64050288e05f725888ad
Marcio Araujo <araujo.marcio@pm.me>
2023-03-08 16:15:56 -0300

    [ENTREGA] Requisito 58
    Implemente a solução de maneira que o e-mail da pessoa usuária deve estar visível

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 function Profile() {
+  const data = JSON.parse(localStorage.getItem('user'));
+  const { email } = data;
   return (
     <div>
-      <h2 data-testid="profile-email"> email </h2>
+      <h2 data-testid="profile-email">
+        {email}
+      </h2>
       <button data-testid="profile-done-btn">Done Recipes</button>
       <button data-testid="profile-favorite-btn">Favorite Recipes</button>
       <button data-testid="profile-logout-btn">Logout</button>
